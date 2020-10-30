877c848696b6d0562f92f969d96c79957f4e922a
haijialiu <894226461@qq.com>
2020-10-30 15:30:02 +0800

    1

--- User/models.py
+++ User/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20,blank=True, null=True)
+    pass_word = models.CharField(max_length = 20)
+    
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
