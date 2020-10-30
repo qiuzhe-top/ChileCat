ae3f16f21ed5684a6d8c3744450ccf2ee7d3183c
秋哲 <2810201146@qq.com>
2020-10-30 23:02:24 +0800

    修改路由

--- Ask/urls.py
+++ Ask/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('leave_type', views.LeaveType.as_view()),
     path('draft', views.Draft.as_view()),
-    path('drafts', views.Drafts.as_view()),
+    # path('drafts', views.Drafts.as_view()), #规划时写错了 具体为draft Put请求
     path('audit', views.Audit.as_view()),
 ]
--- Ask/views.py
+++ Ask/views.py
@@ -47,27 +47,27 @@ class Draft(APIView):
         ret['data'] = 'data'
 
 
-class Drafts(APIView):
-    def post(self, request): 
-        ret = {}
-        ret['code'] = '2000'
-        ret['message'] = '提示信息'
-        ret['data'] = 'data'
-    def get(self,request):
-        ret = {}
-        ret['code'] = '2000'
-        ret['message'] = '提示信息'
-        ret['data'] = 'data'
-    def put(self,request):
-        ret = {}
-        ret['code'] = '2000'
-        ret['message'] = '提示信息'
-        ret['data'] = 'data'
-    def delete(self,request):
-        ret = {}
-        ret['code'] = '2000'
-        ret['message'] = '提示信息'
-        ret['data'] = 'data'
+# class Drafts(APIView):
+#     def post(self, request): 
+#         ret = {}
+#         ret['code'] = '2000'
+#         ret['message'] = '提示信息'
+#         ret['data'] = 'data'
+#     def get(self,request):
+#         ret = {}
+#         ret['code'] = '2000'
+#         ret['message'] = '提示信息'
+#         ret['data'] = 'data'
+#     def put(self,request):
+#         ret = {}
+#         ret['code'] = '2000'
+#         ret['message'] = '提示信息'
+#         ret['data'] = 'data'
+#     def delete(self,request):
+#         ret = {}
+#         ret['code'] = '2000'
+#         ret['message'] = '提示信息'
+#         ret['data'] = 'data'
 
 class Audit(APIView):
     def post(self, request): 
