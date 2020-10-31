4f133c0a91c2f7b1e1518f130d53d8918013a1f3
秋哲 <2810201146@qq.com>
2020-10-31 08:11:28 +0800

    数据变化

--- Ask/views.py
+++ Ask/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
 class LeaveType(APIView):
     def post(self, request): 
--- Career/views.py
+++ Career/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
 class Info(APIView):
     def post(self, request): 
