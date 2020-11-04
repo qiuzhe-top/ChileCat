496c6dfb19e7aea4deb2541d2214a0afa3e729c5
haijialiu <894226461@qq.com>
2020-11-04 09:21:06 +0800

    the first modify "info"

--- Career/views.py
+++ Career/views.py
@@ -5,6 +5,10 @@ from django.http import JsonResponse
 
 
 class Info(APIView):
+    '''
+    http://127.0.0.1:8000/api/career/info
+    page:10 per
+    '''
     def post(self, request):
         ret = {}
         ret['code'] = '2000'
@@ -13,7 +17,24 @@ class Info(APIView):
         return JsonResponse(ret)
 
     def get(self, request):
-        ret = {"demo":1}
+        '''
+        处理get请求
+        '''
+        ret = {
+            'id':0,
+            'title':"on title",
+            'introduction':"on introduction",
+            'body_text':"on more details",
+            'creation_time':"0000.00.00"
+            }
+        redic = request.GET
+        text_id = -1
+        if 'id' in redic:
+            text_id = redic['id']
+        if text_id != -1:
+            #TODO(liuhai): id存在时
+            pass
+
         return JsonResponse(ret)
 
     def put(self, request):
