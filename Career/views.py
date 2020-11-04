232e52c4cdcd0b4efb934e7d345a0fbfa27b0a5b
haijialiu <894226461@qq.com>
2020-11-05 07:49:17 +0800

    nearly completed

--- Career/views.py
+++ Career/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from . import models
+from django.core.paginator import Paginator
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
@@ -20,22 +23,61 @@ class Info(APIView):
         '''
         处理get请求
         '''
-        ret = {
+        redic = request.GET
+        text_id = redic.get('id',-1)
+        if text_id != -1:
+            ret = {
             'id':0,
             'title':"on title",
             'introduction':"on introduction",
             'body_text':"on more details",
             'creation_time':"0000.00.00"
             }
-        redic = request.GET
-        text_id = redic.get('id',-1)
-        if text_id != -1:
-            #TODO(liuhai): id存在时
-            pass
-
-        return JsonResponse(ret)
+            try:
+                ret_info = models.Career.objects.get(id = text_id)
+            except ObjectDoesNotExist as do_not_find:
+                print("no this text",do_not_find)
+                error_message = {'info':"no id = "+text_id+" text!"}
+                return JsonResponse(error_message)
+            ret['id'] = ret_info.id
+            ret['title'] = ret_info.title
+            ret['introduction'] = ret_info.note
+            ret['body_text'] = ret_info.note
+            ret['creation_time'] = ret_info.release_time
+        else:   #id不存在,查询all,并根据page返回第几页
+            rets = {
+                'page':-1,
+                'max_page':-1,
+                'list':[]
+            }
+            ret_page = redic.get('page',-1)
+            if ret_page == -1:
+                ret = {'message':"unexpect request!"}
+                return JsonResponse(ret)
+            else:
+                ret = {
+                    'id':"-1",
+                    'title':"no title",
+                    'body_text':"none",
+                    'creation_time':"0000.00.00",
+                    }
+                rets['page'] = ret_page
+                text_list = models.Career.objects.all()
+                paginator = Paginator(text_list,10) #每页显示十项
+                #TODO(liuhai):如果没有第二页,并且请求了第二页,会raise unexpectaion,需要捕获
+                for i in paginator.page(ret_page):
+                    ret['id'] = i.id
+                    ret['title'] = i.title
+                    ret['body_text'] = i.note
+                    ret['creation_time'] = i.release_time
+                    rets['list'].append(ret)
+                return JsonResponse(rets)
+        return JsonResponse({'info':"unexpect error"})
 
     def put(self, request):
+        '''
+        处理put请求
+        '''
         ret = {}
         ret['code'] = '2000'
         ret['message'] = '提示信息'
@@ -43,6 +85,9 @@ class Info(APIView):
         return JsonResponse(ret)
 
     def delete(self, request):
+        '''
+        处理delete请求
+        '''
         ret = {}
         ret['code'] = '2000'
         ret['message'] = '提示信息'
