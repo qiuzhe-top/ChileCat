f9fa5ed64f1eddf4a2ce65b1f986b7e33e1a6684
haijialiu <894226461@qq.com>
2020-11-05 16:19:38 +0800

    ask phase_1

--- Ask/views.py
+++ Ask/views.py
@@ -4,7 +4,7 @@
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.core.paginator import Paginator
 from .models import Ask
 from . import models
 # Create your views here .
@@ -118,23 +118,56 @@ class Draft(APIView):
                 ret['message'] = "没有此id的请假条"
                 return JsonResponse(ret)
             ret['data'] = {
-                'user_id':ask.user_id,
-                'status':ask.status,
-                'constact_info':ask.constact_info,
-                'ask_type':ask.ask_type,
-                'reason':ask.reason,
-                'place':ask.place,
-                'ask_state':ask.ask_state,
-                'start_time':ask.start_time,
-                'end_time':ask.end_time,
-                'created_time':ask.created_time,
-                'modify_time':ask.modify_time,
+                'user_id': ask.user_id,
+                'status': ask.status,
+                'constact_info': ask.constact_info,
+                'ask_type': ask.ask_type,
+                'reason': ask.reason,
+                'place': ask.place,
+                'ask_state': ask.ask_state,
+                'start_time': ask.start_time,
+                'end_time': ask.end_time,
+                'created_time': ask.created_time,
+                'modify_time': ask.modify_time,
             }
             return JsonResponse(ret)
         else:
-            #TODO(liuhai) 分页查询
-            pass
+            req_page = int(req_list.get('page',-1))
+            if req_page == 0:
+                req_page =1
+            print(req_page)
+            if req_page == -1:
+                ret['code'] = 4000
+                ret['message'] = "错误的请求方式,至少有'page','id'其中之一"
+                return JsonResponse(ret)
+            ret['data'] = {'list':[]}
+            ask_unit = {
+                'text': "请假类型+15字简介",
+		        'start_time': "开始时间",
+		        'vacate_time': "请假时长",
+		        'state': "状态",
+		        'state_level': "目前审核状态"
+            }
+            ask_all_list = models.Ask.objects.all()
+            paginator = Paginator(ask_all_list,10)
+            max_page = paginator.num_pages
+            print(paginator.num_pages)
+            if max_page < 1:
+                ret['code'] = 4000
+                ret['message'] = "没有任何数据"
+                return JsonResponse(ret)
+            if req_page > max_page:
+                req_page = max_page
+            for i in paginator.page(req_page):
+                ask_unit['text'] = i.ask_type
+                ask_unit['start_time'] = i.start_time
+                ask_unit['vacate_time'] = i.end_time - i.start_time
+                ask_unit['state'] = i.status
+                ask_unit['state_level'] = i.ask_state
+                ret['page']['list'].append(ask_unit)
+            ret['page'] = req_page
         return JsonResponse(ret)
+        #TODO(liuhai) 写到 获取多个请假条简单信息 分页 获取详细信息,剩下自己加油
 
     def put(self, request):
         '''
