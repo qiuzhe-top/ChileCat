89578b9cfc1d2e0517d87adac298550a890edace
qiuzhe <2810201146@qq.com>
2020-12-19 18:10:42 +0800

    配置远程数据库

--- ChileCat/settings.py
+++ ChileCat/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'ChileCat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'chilecat',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chilecat',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '47.102.215.230',
+        'PORT': '3306'
+    }
+}
 
 
 # Password validation
