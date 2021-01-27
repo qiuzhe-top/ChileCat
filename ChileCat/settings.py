ec24f21c281ca08c272092dd11382c049b9ea129
qiuzhe <2810201146@qq.com>
2021-01-27 09:19:27 +0800

    .gitignore 更新

--- ChileCat/settings.py
+++ ChileCat/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
+import os,sys
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+sys.path.insert(0,os.path.join(BASE_DIR,'Apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
