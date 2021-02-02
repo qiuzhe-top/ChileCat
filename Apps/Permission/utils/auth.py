df86276c3d37cc30ac2dd022502c1662af5390cf
qiuzhe <2810201146@qq.com>
2021-02-02 14:45:56 +0800

    Api 接口认证

--- Apps/Permission/utils/auth.py
+++ Apps/Permission/utils/auth.py
@@ -2,41 +2,27 @@
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication
 from Apps.User import models as UserModel
-from Apps.Permission.models import ApiPermission
 from django.contrib.auth.models import AnonymousUser
 # from Apps.Permission.models import ApiPermission
-# 全局登录验证
+# 
 class AuthPermission(BaseAuthentication):
     '''
-        所有接口和操作默认需要身份验证
+        全局身份标识
     '''
     def authenticate(self,request):
-        url = request.META['PATH_INFO']
-        method = request.META['REQUEST_METHOD']
-        #TODO(liuhai) 白名单返回None用户为后台的permission做权限判断(注意这是不稳妥的)
+        token = request.META.get("HTTP_TOKEN")
+        token_obj = UserModel.Token.objects.filter(token = token).first()
+        if not token_obj:
+            return (AnonymousUser,None)
+        return (token_obj.user.django_user,None)
 
-        # print("登录验证：全局登录验证")
-        return token_auth_function(request)
-
-# 身份登录验证逻辑
-def token_auth_function(request):
-    '''身份登录验证逻辑'''
-    token = request.META.get("HTTP_TOKEN")
-    token_obj = UserModel.Token.objects.filter(token = token).first()
-    url = request.META['PATH_INFO']
-    method = request.META['REQUEST_METHOD']
-    if url_write_list(url,method):
-        return (AnonymousUser,None)
-    if not token_obj:
-        raise exceptions.AuthenticationFailed('用户认证失败')
-    return (token_obj.user.django_user,None)
-
-def url_write_list(url,method):
-    '''判断访问的url是否在白名单(只判断url)'''
-    url = ApiPermission.objects.filter(url=url,method=method)
-    if url.exists():
-        url_per = url.first().per_id.role.filter(name="白名单")
-        if url_per.exists():
-            return True
-        return False
-    return False
+class AuthPer(BaseAuthentication):
+    '''
+        局部身份认证
+    '''
+    def authenticate(self,request):
+        token = request.META.get("HTTP_TOKEN")
+        token_obj = UserModel.Token.objects.filter(token = token).first()
+        if not token_obj:
+            raise exceptions.AuthenticationFailed('用户认证失败')
+        return (token_obj.user.django_user,None)
--- Apps/Permission/utils/permission.py
+++ Apps/Permission/utils/permission.py
@@ -2,40 +2,52 @@
 from rest_framework.permissions import BasePermission
 # from rest_framework import exceptions
 # from Apps.User import models as UserModel
-# from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser
 from Apps.Permission import models
-from Apps.Permission.utils.auth import url_write_list
+from Apps.Permission.models import ApiPermission
+
+# from Apps.Permission.utils.auth import url_write_list
 # 全局API接口权限
-class ApiPermission(BasePermission):
+class ApiPublicPermission(BasePermission):
     '''
         api权限,用来验证用户是否有权限访问对应的api
     '''
-    message = '访问接口失败'
+    message = '没有权限'
     def has_permission(self, request, view):
         """
         只有拥有当前api权限的用户通过
         """
         url = request.META['PATH_INFO']
         method = request.META['REQUEST_METHOD']
-        print(request.user)
+        
         if url_write_list(url,method):
             return True
+            
+        if request.user == AnonymousUser:
+            self.message = '未登录'
+            return False 
+
         control_obj = models.ApiPermission.objects.filter(url=url,method=method)
-        try:
-            if request.user.user.userinfo.user_role.filter(name="root").exists():
-                return True
-            if not control_obj.exists():
-                ApiPermission.message = "此接口没有开放权限,请联系管理员"
-                print("此接口没有开放权限,请联系管理员")
-                return False
-            if request.user.user.userinfo.user_role.filter(
-                role_permit=control_obj.first().per_id
-                ).exists():
-                return True
-            ApiPermission.message="没有权限"
+        if request.user.user.userinfo.user_role.filter(name="root").exists():
+            return True
+        if not control_obj.exists():
+            self.message = "此接口没有开放权限,请联系管理员"
+            print("此接口没有开放权限,请联系管理员")
             return False
-        except AttributeError:
-            ApiPermission.message = "用户未登录"
-            return False
-        print("接口权限：全局API接口权限")
-        return True
+        if request.user.user.userinfo.user_role.filter(
+            role_permit=control_obj.first().per_id
+            ).exists():
+            return True
+        return False
+
+def url_write_list(url,method):
+    '''
+        判断访问的url是否在白名单(判断url+请求方式)
+    '''
+    url = ApiPermission.objects.filter(url=url,method=method)
+    if url.exists():
+        url_per = url.first().per_id.role.filter(name="白名单")
+        if url_per.exists():
+            return True
+        return False
+    return False
