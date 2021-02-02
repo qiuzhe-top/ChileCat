ae1ddcb10e10d6654450389522337441574df851
haijialiu <894226461@qq.com>
2021-02-02 16:00:08 +0800

    the least

--- Apps/Permission/utils/auth.py
+++ Apps/Permission/utils/auth.py
@@ -1,9 +1,9 @@
 '''用户认证'''
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication
+from django.contrib.auth.models import AnonymousUser
 from Apps.User import models as UserModel
 from Apps.Permission.models import ApiPermission
-from django.contrib.auth.models import AnonymousUser
 # from Apps.Permission.models import ApiPermission
 # 全局登录验证
 class AuthPermission(BaseAuthentication):
@@ -14,7 +14,6 @@ class AuthPermission(BaseAuthentication):
         url = request.META['PATH_INFO']
         method = request.META['REQUEST_METHOD']
         #TODO(liuhai) 白名单返回None用户为后台的permission做权限判断(注意这是不稳妥的)
-
         # print("登录验证：全局登录验证")
         return token_auth_function(request)
 
