60053ab3c1f7bcfb47ee926e88d66181b7189f1f
秋哲 <2810201146@qq.com>
2021-05-27 21:40:01 +0800

    日常

--- ChileCat/urls.py
+++ ChileCat/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from Manage.views import Index
-
+from . import views
 
 urlpatterns = [
     # path('', Index.as_view()),
