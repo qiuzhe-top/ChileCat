f92acbff1089afe33529bea82422cb5941bee7ce
qiuzhe <2810201146@qq.com>
2021-06-24 21:27:58 +0800

    V2.0 测试

--- Apps/SchoolAttendance/admin.py
+++ Apps/SchoolAttendance/admin.py
@@ -2,83 +2,83 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(UserCall)
-class UserCall(admin.ModelAdmin):
-    """楼层"""
-    list_display = (
-        "id", "task", "user", "rule"
-    )
-
-
-@admin.register(TaskFloorStudent)
-class TaskFloorStudent(admin.ModelAdmin):
-    """房间学生信息"""
-    list_display = (
-        "id", "task", "user", "flg"
-    )
-
-
-@admin.register(RoomHistory)
-class RoomHistoryTeleAdmin(admin.ModelAdmin):
-    """寝室访问记录"""
-    list_display = (
-        "id", "room", "task", "created_time"
-    )
-
-
-# @admin.register(TaskClass)
-# class TaskClass(admin.ModelAdmin):
+# @admin.register(UserCall)
+# class UserCall(admin.ModelAdmin):
+#     """楼层"""
 #     list_display = (
-#         "id", "task", "grade"
+#         "id", "task", "user", "rule"
 #     )
 
 
-# @admin.register(TaskFloor)
-# class TaskFloor(admin.ModelAdmin):
+# @admin.register(TaskFloorStudent)
+# class TaskFloorStudent(admin.ModelAdmin):
+#     """房间学生信息"""
 #     list_display = (
-#         "id", "task", "building"
+#         "id", "task", "user", "flg"
 #     )
 
 
-@admin.register(TaskPlayer)
-class TaskPlayer(admin.ModelAdmin):
-    list_display = (
-        "id", "task", "user", "is_admin"
-    )
-    raw_id_fields = ['user',]
+# @admin.register(RoomHistory)
+# class RoomHistoryTeleAdmin(admin.ModelAdmin):
+#     """寝室访问记录"""
+#     list_display = (
+#         "id", "room", "task", "created_time"
+#     )
+
+
+# # @admin.register(TaskClass)
+# # class TaskClass(admin.ModelAdmin):
+# #     list_display = (
+# #         "id", "task", "grade"
+# #     )
+
+
+# # @admin.register(TaskFloor)
+# # class TaskFloor(admin.ModelAdmin):
+# #     list_display = (
+# #         "id", "task", "building"
+# #     )
+
+
+# @admin.register(TaskPlayer)
+# class TaskPlayer(admin.ModelAdmin):
+#     list_display = (
+#         "id", "task", "user", "is_admin"
+#     )
+#     raw_id_fields = ['user',]
 
 
 
-@admin.register(Task)
-class Task(admin.ModelAdmin):
-    """任务管理"""
-    list_display = (
-        "id", "user", "is_open", "types", "user"
-    )
-    raw_id_fields = ['user',]
+# @admin.register(Task)
+# class Task(admin.ModelAdmin):
+#     """任务管理"""
+#     list_display = (
+#         "id", "user", "is_open", "types", "user"
+#     )
+#     raw_id_fields = ['user',]
 
-    filter_horizontal=['grades',]
-
-
-@admin.register(Rule)
-class Rule(admin.ModelAdmin):
-    """规则一级分类"""
-    list_display = (
-        "id", "name", "message"
-    )
+#     filter_horizontal=['grades',]
 
 
-@admin.register(RuleDetails)
-class RuleDetails(admin.ModelAdmin):
-    """规则二级分类"""
-    list_display = (
-        "id", 'name', 'score'
-    )
+# @admin.register(Rule)
+# class Rule(admin.ModelAdmin):
+#     """规则一级分类"""
+#     list_display = (
+#         "id", "name", "message"
+#     )
 
 
-@admin.register(Record)
-class Record(admin.ModelAdmin):
-    """任务记录"""
-    list_display = (
-        "id", "task", "rule", "rule_str", "score","grade_str","student_approved","worker","manager","star_time","last_time"
-    )
+# @admin.register(RuleDetails)
+# class RuleDetails(admin.ModelAdmin):
+#     """规则二级分类"""
+#     list_display = (
+#         "id", 'name', 'score'
+#     )
+
+
+# @admin.register(Record)
+# class Record(admin.ModelAdmin):
+#     """任务记录"""
+#     list_display = (
+#         "id", "task", "rule", "rule_str", "score","grade_str","student_approved","worker","manager","star_time","last_time"
+#     )
