ba0956bef28eeb59521f55e9fe73e4db4fe26089
qiuzhe <2810201146@qq.com>
2021-07-14 15:58:35 +0800

    日更

--- Apps/SchoolAttendance/tests.py
+++ Apps/SchoolAttendance/tests.py
@@ -1,19 +1,29 @@
+'''
+Author: 邹洋
+Date: 2021-05-20 08:37:12
+Email: 2810201146@qq.com
+LastEditors:  
+LastEditTime: 2021-07-14 15:02:55
+Description: 
+'''
+from Apps.User.models import Token
 from django.contrib.auth.models import User
+from django.test import TestCase
 from rest_framework.test import RequestsClient
 
-from Apps.User.models import Token
-from django.test import TestCase
 from . import models
+
+
 # Create your tests here.
 class Task(TestCase):
     # 考勤任务测试类
     def setUp(self) -> None:
         self.username = '195101'
-     
+
         self.api = '/api/attendance/'
         self.http_api = 'http://127.0.0.1:8000' + self.api
 
-        task = models.Task.objects.create(is_open = True, types = '0')
+        task = models.Task.objects.create(is_open=True, types='0')
         u = User.objects.create_user(username=self.username)
         u.save()
 
@@ -23,7 +33,6 @@ class Task(TestCase):
     def get_user(self):
         return User.objects.get(username=self.username)
 
-
     def set_token(self):
         user = self.get_user()
         token = 'alsdjooyzxkhalsdjooyzxkhalsdjooyzxkhalsdjooyzxkh'
@@ -31,7 +40,7 @@ class Task(TestCase):
         self.client.headers.update({'TOKEN': token})
 
     def test_task_obtain(self):
-        data = {'type1':'0'}
-        response =1#self.client.post(self.http_api+'task/obtain',data).json()
+        data = {'type1': '0'}
+        response = 1  # self.client.post(self.http_api+'task/obtain',data).json()
         print(response)
         # self.assertEqual(response['code'],5007)
