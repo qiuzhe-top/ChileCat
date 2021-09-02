e429f56d5bbb9fa00f5baacbf7e21d4343fb1005
qiuzhe <2810201146@qq.com>
2021-09-02 13:35:17 +0800

    尝试云存储上传

--- Manage/views.py
+++ Manage/views.py
@@ -639,5 +639,34 @@ class Apitouviews(CoolBFFAPIView):
 #         return JsonResponse(ret)
 
 
+
+from qiniu import Auth
+@site
+class upload(CoolBFFAPIView):
+    name = _('XXX')
+   
+    def get_context(self, request, *args, **kwargs):
+        #需要填写你的 Access Key 和 Secret Key
+        access_key = 'wXCStxg9VlC2fbUlV3CRIE4sxISYaRxHwVsWwXJX'
+        secret_key = 'qyZEYCbth2fHMNFMXRHc09l4_bss2YvSpu4O4O6N'
+        #构建鉴权对象
+        q = Auth(access_key, secret_key)
+        #要上传的空间
+        bucket_name = 'zhcy-zjjt'
+        #上传后保存的文件名
+        key = None
+        #生成上传 Token，可以指定过期时间等
+        # 上传策略示例
+        # https://developer.qiniu.com/kodo/manual/1206/put-policy
+        policy = {
+        # 'callbackUrl':'https://requestb.in/1c7q2d31',
+        # 'callbackBody':'filename=$(fname)&filesize=$(fsize)'
+        # 'persistentOps':'imageView2/1/w/200/h/200'
+        }
+        #3600为token过期时间，秒为单位。3600等于一小时
+        token = q.upload_token(bucket_name, key, 3600, policy)
+        print(token)
+        return token
+
 urls = site.urls
 urlpatterns = site.urlpatterns
