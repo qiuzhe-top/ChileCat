e9b83a7923280d6649169fdc6135d4bc4075b39e
qiuzhe <2810201146@qq.com>
2021-09-02 14:58:58 +0800

    添加作业助手模块相关代码

--- Apps/HomeWork/admin.py
+++ Apps/HomeWork/admin.py
@@ -1,3 +1,15 @@
+'''
+Author: 邹洋
+Date: 2021-09-02 14:36:25
+Email: 2810201146@qq.com
+LastEditors:  
+LastEditTime: 2021-09-02 14:47:22
+Description: 
+'''
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(HomeWork)
+class HomeWork(admin.ModelAdmin):
+    """任务管理"""
+    # list_display = '__all__'
--- Apps/HomeWork/models.py
+++ Apps/HomeWork/models.py
@@ -3,12 +3,16 @@ Author: 邹洋
 Date: 2021-09-02 14:35:30
 Email: 2810201146@qq.com
 LastEditors:  
-LastEditTime: 2021-09-02 14:35:31
+LastEditTime: 2021-09-02 14:57:20
 Description: 
 '''
 from django.db import models
 from django.contrib.auth.models import User
+from cool.admin import admin_register
 
+
+
+admin_register()
 class HomeWork(models.Model):
     name = models.CharField(max_length=100, verbose_name=u'名称')
     message = models.CharField(max_length=1000, verbose_name=u'备注')
@@ -19,7 +23,7 @@ class HomeWork(models.Model):
 
     class Meta:
         verbose_name_plural = "作业任务"
-
+admin_register()
 class DoWork(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     work = models.ForeignKey(HomeWork, on_delete=models.CASCADE)
