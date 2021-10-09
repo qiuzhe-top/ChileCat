ab19656420ca942f5dd5054f11be26129ced4404
qiuzhe <2810201146@qq.com>
2021-10-09 10:30:30 +0800

    解决按默认时间筛选BUG

--- Apps/SchoolAttendance/views.py
+++ Apps/SchoolAttendance/views.py
@@ -3,7 +3,7 @@ Author: 邹洋
 Date: 2021-05-20 08:37:12
 Email: 2810201146@qq.com
 LastEditors:  
-LastEditTime: 2021-09-26 19:24:46
+LastEditTime: 2021-10-09 10:26:45
 Description: 
 '''
 import datetime
@@ -11,6 +11,7 @@ import datetime
 from Apps.SchoolAttendance.pagination import RecordQueryrPagination
 from Apps.User.utils.auth import get_token_by_user
 from cool.views import CoolAPIException, CoolBFFAPIView, ErrorCode, ViewSite
+from core.common import get_end_date, get_start_date
 from core.excel_utils import at_all_out_xls, excel_to_list, out_knowing_data
 from core.query_methods import Concat
 from core.settings import *
@@ -158,12 +159,8 @@ class Condition(TaskBase):
 
     def get_context(self, request, *args, **kwargs):
         # 时间区间
-        start_date = request.params.start_date
-        end_date = request.params.end_date
-        end_date = datetime.datetime(
-            end_date.year, end_date.month, end_date.day, 23, 59, 59
-        )
-
+        start_date = get_start_date(request)
+        end_date = get_end_date(request)
         # 楼-层筛选
         building = request.params.building
         floor = request.params.floor
@@ -186,13 +183,11 @@ class Condition(TaskBase):
 
         return serializers.ConditionRecord(records, request=request, many=True).data
     class Meta:
-        now = datetime.datetime.now()
-        t = datetime.datetime(now.year, now.month, now.day)
         param_fields = (
             ('building', fields.CharField(label=_('楼'),default=None)),
             ('floor', fields.CharField(label=_('层'),default=None)),
-            ('start_date', fields.DateField(label=_('开始日期'), default=t)),
-            ('end_date', fields.DateField(label=_('结束日期'), default=t)),
+            ('start_date', fields.DateField(label=_('开始日期'), default=None)),
+            ('end_date', fields.DateField(label=_('结束日期'), default=None)),
         )
 
 
@@ -669,11 +664,8 @@ class OutData(CoolBFFAPIView):
 
         username = request.params.username
         college_id = request.params.college_id
-        start_date = request.params.start_date
-        end_date = request.params.end_date
-        end_date = datetime.datetime(
-            end_date.year, end_date.month, end_date.day, 23, 59, 59
-        )
+        start_date = get_start_date(request)
+        end_date = get_end_date(request)
 
         # 筛选条件
         q1 = Q(manager__isnull=True)  # 是否销假
@@ -774,14 +766,11 @@ class OutData(CoolBFFAPIView):
         return at_all_out_xls(records)
 
     class Meta:
-        now = datetime.datetime.now()
-        t = datetime.datetime(now.year, now.month, now.day)
-        t = datetime.datetime.strftime(t, "%Y-%m-%d")
         param_fields = (
             ('username', fields.CharField(label=_('用户名'), default=None)),
             ('college_id', fields.IntegerField(label=_('分院ID'), default=None)),
-            ('start_date', fields.DateField(label=_('开始日期'), default=t)),
-            ('end_date', fields.DateField(label=_('结束日期'), default=t)),
+            ('start_date', fields.DateField(label=_('开始日期'), default=None)),
+            ('end_date', fields.DateField(label=_('结束日期'), default=None)),
         )
 
 
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  * @Date: 2021-07-04 13:27:27
  * @Email: 2810201146@qq.com
  * @LastEditors:  
- * @LastEditTime: 2021-09-30 13:42:23
+ * @LastEditTime: 2021-10-09 10:30:24
  * @Description: 
 -->
 # 智慧彩云
@@ -15,6 +15,7 @@
 - 优化excel转列表选择是否处理第一行
 - 初始化系统时 创建自定义规则 的数据记录
 - 优化 DormRoomInfo 当寝室没人时不在就不输出这个寝室
+- 解决按默认时间筛选BUG
 ### V 1.1.0
 - 继承User拷贝UserAdmin完成注册
 - 完成自定义User移植
