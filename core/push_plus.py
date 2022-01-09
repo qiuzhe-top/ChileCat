ad4d6c62fceef0a48f3501244baf133c4183bf1b
qiuzhe <2810201146@qq.com>
2022-01-09 09:11:30 +0800

    设置微信推送条件为 DEBUG==FALSE

--- core/push_plus.py
+++ core/push_plus.py
@@ -3,19 +3,20 @@ Author: 邹洋
 Date: 2021-12-14 10:09:15
 Email: 2810201146@qq.com
 LastEditors:  
-LastEditTime: 2021-12-14 12:13:47
+LastEditTime: 2022-01-09 09:10:19
 Description: 公众号消息推送
 '''
 
 import requests
-
+from ChileCat.settings import DEBUG
 token = '10218bcad82e4ddfb842b47d358334fa'
 url = 'http://pushplus.hxtrip.com/send'
 def push_wx(title,content,template='html'):
-    data = {
-        'token':token,
-        'title':title,
-        'content':content,
-        'template':template
-    }
-    requests.post(url,data)
+    if DEBUG == False:
+        data = {
+            'token':token,
+            'title':title,
+            'content':content,
+            'template':template
+        }
+        requests.post(url,data)
