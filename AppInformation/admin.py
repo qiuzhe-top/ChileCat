d1a11d530a07c63c91ab1ce308ca813199ba015b
qiuzhe <2810201146@qq.com>
2022-02-23 11:13:17 +0800

    去除不用引用

--- AppInformation/admin.py
+++ AppInformation/admin.py
@@ -3,13 +3,10 @@ Author: 邹洋
 Date: 2022-02-07 11:26:10
 Email: 2810201146@qq.com
 LastEditors:  
-LastEditTime: 2022-02-23 11:08:43
+LastEditTime: 2022-02-23 11:13:04
 Description: 
 '''
 from django.contrib import admin
-from django.http import JsonResponse
-
-from Core.common.excel import ExcelBase
 from .models import *
 
 @admin.register(Grade)
