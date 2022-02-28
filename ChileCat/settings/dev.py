6a85643568ed73148f6ca929b9ee78c6630cdb03
qiuzhe <2810201146@qq.com>
2022-02-28 15:23:34 +0800

    配置开发模式

--- ChileCat/settings/dev.py
+++ ChileCat/settings/dev.py
@@ -3,11 +3,14 @@ Author: 邹洋
 Date: 2022-02-12 22:12:23
 Email: 2810201146@qq.com
 LastEditors:  
-LastEditTime: 2022-02-23 10:43:13
+LastEditTime: 2022-02-28 15:23:19
 Description: 线上环境
 '''
 from .base import *
 DEBUG = True
+ASGI_APPLICATION = 'ChileCat.asgidev.application'
+
+
 password = 'Zhou24272592.' 
 host = '127.0.0.1'
  
