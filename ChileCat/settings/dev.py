4167763cfe45f4deb29b9cbebc6b802dbd3cfa1c
qiuzhe <2810201146@qq.com>
2022-03-03 20:16:00 +0800

    修护 批量违纪记录 批量销假

--- ChileCat/settings/dev.py
+++ ChileCat/settings/dev.py
@@ -3,7 +3,7 @@ Author: 邹洋
 Date: 2022-02-12 22:12:23
 Email: 2810201146@qq.com
 LastEditors:  
-LastEditTime: 2022-02-23 10:43:13
+LastEditTime: 2022-03-03 19:25:13
 Description: 线上环境
 '''
 from .base import *
@@ -43,7 +43,7 @@ CACHES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ChileCat',
+        'NAME': 'ChileCatTest',
         'USER': 'root',
         'PASSWORD': sql_password,
         'HOST': sql_host,
